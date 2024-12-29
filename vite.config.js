bfbff9fcc40280c87ccef239af35d556cc89c5c6
billjeferson <billnbbn@gmail.com>
2024-12-30 02:25:06 +0700

    new path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portofolio-react-tailwindcss/', // Sesuaikan dengan subfolder jika diperlukan
-  plugins: [react()],
+  base: "/", // Ubah menjadi '/' jika berada di root domain
 });
